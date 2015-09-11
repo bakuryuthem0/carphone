0360ff927de02e67afb7e673ec7ae9f1cc7ba11d
carlos <carlos.salazar@ingenium-dv.com>
2015-09-11 13:26:47 -0430

    layout modified

--- public/app/views/layouts/default.blade.php
+++ public/app/views/layouts/default.blade.php
@@ -7,14 +7,13 @@
     <meta name="author" content="">
     <title>{{ $title }}</title>
 	{{ HTML::style('css/bootstrap.min.css') }}
-	{{ HTML::style('css/font-awesome.min.css') }}
 	{{ HTML::style('css/prettyPhoto.css') }}
 	{{ HTML::style('css/price-range.css') }}
 	{{ HTML::style('css/animate.css') }}
 	{{ HTML::style('css/main.css') }}
 	{{ HTML::style('css/responsive.css') }}
 	{{ HTML::style('css/custom.css') }}
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
@@ -28,10 +27,10 @@
 
 <body>
 	<header id="header"><!--header-->
-		<div class="header_top"><!--header_top-->
+		<div class="header_top bg-info"><!--header_top-->
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-6">
+					<div class="col-md-6">
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
 								<li><a href="#"><i class="fa fa-phone"></i> Telefono</a></li>
@@ -39,7 +38,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-6">
+					<div class="col-md-6">
 						<div class="social-icons pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -57,7 +56,7 @@
 		<div class="header-middle"><!--header-middle-->
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-4">
+					<div class="col-md-4">
 						<div class="logo pull-left">
 							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
 						</div>
@@ -85,7 +84,7 @@
 							</div> --}}
 						</div>
 					</div>
-					<div class="col-sm-8">
+					<div class="col-md-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								@if(Auth::check())
@@ -93,7 +92,7 @@
 									<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Pagar</a></li>
 									<li><a href="cart. html"><i class="fa fa-shopping-cart"></i> Carrito</a></li>
 								@else
-									<li><a href="#"><i class="fa fa-user"></i> Registrarse</a></li>
+									<li><a href="{{ URL::to('registrarse') }}"><i class="fa fa-user"></i> Registrarse</a></li>
 									<li><a href="{{ URL::to('iniciar-sesion') }}"><i class="fa fa-lock"></i> Login</a></li>
 								@endif
 
@@ -107,7 +106,7 @@
 		<div class="header-bottom"><!--header-bottom-->
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-9">
+					<div class="col-md-9">
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 								<span class="sr-only">Toggle navigation</span>
@@ -119,7 +118,8 @@
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
 								<li><a href="index.html" class="active">Inicio</a></li>
-								<li class="dropdown"><a href="#">Tiendas<i class="fa fa-angle-down"></i></a>
+								<li class="dropdown">
+									<a href="#">Tiendas<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
                                         <li><a href="shop.html">Carrors</a></li>
 										<li><a href="product-details.html">Telefonos</a></li> 
@@ -141,9 +141,12 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
+					<div class="col-md-3">
 						<div class="search_box pull-right">
-							<input type="text" placeholder="Search"/>
+							<div class="input-group">
+							  <span class="input-group-addon" id="basic-addon1"><i class="fa fa-search"></i></span>
+							  <input type="text" class="form-control" placeholder="Buscar" aria-describedby="basic-addon1">
+							</div>
 						</div>
 					</div>
 				</div>
@@ -153,91 +156,12 @@
 	
 	@yield('content');
 	
-	<footer id="footer"><!--Footer-->
-		<div class="footer-top">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-2">
-						<div class="companyinfo">
-							<h2><span>e</span>-shopper</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
-						</div>
-					</div>
-					<div class="col-sm-7">
-						<div class="col-sm-3">
-							<div class="video-gallery text-center">
-								<a href="#">
-									<div class="iframe-img">
-										<img src="images/home/iframe1.png" alt="" />
-									</div>
-									<div class="overlay-icon">
-										<i class="fa fa-play-circle-o"></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class="col-sm-3">
-							<div class="video-gallery text-center">
-								<a href="#">
-									<div class="iframe-img">
-										<img src="images/home/iframe2.png" alt="" />
-									</div>
-									<div class="overlay-icon">
-										<i class="fa fa-play-circle-o"></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class="col-sm-3">
-							<div class="video-gallery text-center">
-								<a href="#">
-									<div class="iframe-img">
-										<img src="images/home/iframe3.png" alt="" />
-									</div>
-									<div class="overlay-icon">
-										<i class="fa fa-play-circle-o"></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class="col-sm-3">
-							<div class="video-gallery text-center">
-								<a href="#">
-									<div class="iframe-img">
-										<img src="images/home/iframe4.png" alt="" />
-									</div>
-									<div class="overlay-icon">
-										<i class="fa fa-play-circle-o"></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-3">
-						<div class="address">
-							<img src="images/home/map.png" alt="" />
-							<p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
+	<footer id="footer" class="bg-info"><!--Footer-->
+				
 		<div class="footer-widget">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-2">
+					<div class="col-md-2">
 						<div class="single-widget">
 							<h2>Service</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -249,7 +173,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-2">
+					<div class="col-md-2">
 						<div class="single-widget">
 							<h2>Quock Shop</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -261,7 +185,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-2">
+					<div class="col-md-2">
 						<div class="single-widget">
 							<h2>Policies</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -273,7 +197,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-2">
+					<div class="col-md-2">
 						<div class="single-widget">
 							<h2>About Shopper</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -285,12 +209,16 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3 col-sm-offset-1">
+					<div class="col-md-3 col-md-offset-1">
 						<div class="single-widget">
 							<h2>About Shopper</h2>
-							<form action="#" class="searchform">
-								<input type="text" placeholder="Your email address" />
-								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+							<form action="#" class="searchform form-inline">
+								<div>
+									<div class="form-group">
+										<input type="text" placeholder="Your email address" / class="input form-control">
+									</div>
+									<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								</div>
 								<p>Get the most recent updates from <br />our site and be updated your self...</p>
 							</form>
 						</div>
