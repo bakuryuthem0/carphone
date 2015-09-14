5f8cc7d4505cda84cf98df2ea3ce67eb33be5840
Carlucho <shenlong_12@hotmailcom>
2015-09-14 08:34:00 -0430

    database modified phones table added

--- public/carphone.sql
+++ public/carphone.sql
@@ -1,47 +1,86 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Versión 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.5.42)
-# Base de datos: carphone
-# Tiempo de Generación: 2015-09-10 16:57:35 -0430
-# ************************************************************
-
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: carphone
+-- ------------------------------------------------------
+-- Server version	5.5.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `phones`
+--
 
-# Volcado de tabla users
-# ------------------------------------------------------------
+DROP TABLE IF EXISTS `phones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `deleted` int(11) DEFAULT '0',
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phones`
+--
+
+LOCK TABLES `phones` WRITE;
+/*!40000 ALTER TABLE `phones` DISABLE KEYS */;
+INSERT INTO `phones` VALUES (1,'Alcatel',0,'2015-09-14','2015-09-14'),(2,'htc',0,'2015-09-14','2015-09-14'),(3,'kyocera',0,'2015-09-14','2015-09-14'),(4,'lg',0,'2015-09-14','2015-09-14'),(5,'microsoft',0,'2015-09-14','2015-09-14'),(6,'samsung',0,'2015-09-14','2015-09-14'),(7,'zte',0,'2015-09-14','2015-09-14'),(8,'nokia',0,'2015-09-14','2015-09-14');
+/*!40000 ALTER TABLE `phones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL,
   `username` varchar(30) COLLATE latin1_general_ci DEFAULT NULL,
   `password` varchar(100) COLLATE latin1_general_ci DEFAULT NULL,
+  `email` varchar(30) COLLATE latin1_general_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE latin1_general_ci DEFAULT NULL,
   `created_at` date DEFAULT NULL,
   `updated_at` date DEFAULT NULL,
   `deleted` int(11) DEFAULT '0',
+  `role` int(11) DEFAULT '0',
+  `user_deleted` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'prueba','$2y$08$pVfjyr8ySd/5WOW/1SseQOdCAUI2JazbZfvCB97Yi7bY/idDb0fR.','email@email.com','OukGBaYLZTV2TIm57NoNJMKmuWejRTw7JmGI9GrLoCUlJ1F5K4Isas7ZUx52','2015-09-13','2015-09-13',0,0,0),(2,'admin','$2y$08$pVfjyr8ySd/5WOW/1SseQOdCAUI2JazbZfvCB97Yi7bY/idDb0fR.',NULL,'e72oud1BOYZSSZ3Jy2QMxI0TeTiZvwy9zWoWYhUJgW5HGOoQbAGrm0IeoCYK',NULL,NULL,0,1,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-09-14  8:32:37
